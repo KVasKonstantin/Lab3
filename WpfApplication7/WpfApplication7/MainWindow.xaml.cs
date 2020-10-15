1a782fc60738c29b1ab7865a125e2cbe4c5de98a
KVasKonstantin <72942091+KVasKonstantin@users.noreply.github.com>
2020-10-15 21:31:34 +0300

    Add files via upload

---
+++ WpfApplication7/WpfApplication7/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton_Checked(object color, RoutedEventArgs e)
+        {
+
+        }
+
+        private void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            if (rb1.IsChecked == true)
+            {
+                Background = (comboBox.SelectedItem as ComboBoxItem).Background;
+            }
+            else if(rb2.IsChecked==true)
+            {
+                byte R = byte.Parse(tbr.Text);
+
+                byte G = byte.Parse(tbg.Text);
+
+                byte B = byte.Parse(tbb.Text);
+
+                Background = new SolidColorBrush(Color.FromArgb(255, R,G,B));
+            }
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
